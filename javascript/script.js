511837c7f095158a30856e96d9071743c50d8daf
arlet.exe <arletyjack@gmail.com>
2026-09-22 09:40:57 -0600

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+const name = "Adalberto Lopez";
+
+let materia = "Diseño Web";
+let proyecto = "Proyecto Final";
+let age = 33;
+
+age = 34;
+
+function mostrarMateria() {
+    console.log("Materia a mostrar originalmente es: " + materia);
+}
+
+function mostrarVariable(Variable) {
+    console.log("Variable a mostrar: " + Variable);
+}
+
+function textoAMayusculas(texto) {
+    return texto.toUpperCase();
+}
